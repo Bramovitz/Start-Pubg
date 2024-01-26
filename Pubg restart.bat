0a6f8e0d8d217fa9b913ab22c3acd8788fa07abc
Bramovitz <101013763+Bramovitz@users.noreply.github.com>
2024-01-26 16:58:05 +0100

    Add files via upload

---
+++ Pubg restart.bat
@@ -0,0 +1,12 @@
+taskkill /f /im TslGame.exe
+taskkill /f /im TslGame_BE.exe
+taskkill /f /im ExecPubg.exe
+taskkill /f /im TslGame.exe
+
+start steam://rungameid/578080
+
+timeout /t 90
+
+taskkill /f /im TslGame_BE.exe
+taskkill /f /im ExecPubg.exe
+taskkill /f /im zksvc.exe
